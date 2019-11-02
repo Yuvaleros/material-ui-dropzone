e84318f23a5e03d1d1c8ac035a0d4b8d8d9e22dc
Jason Luboff <jluboff@gmail.com>
2019-11-02 13:28:49 -0700

    Updated type definitions to inclide getFile/getDrop message

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,46 +1,54 @@
-import * as React from "react";
+import * as React from 'react';
 
 export interface DropzoneAreaProps {
-    acceptedFiles?: string[];
-    filesLimit?: number;
-    maxFileSize?: number;
-    dropzoneText?: string;
-    showPreviews?: boolean;
-    showPreviewsInDropzone?: boolean;
-    showFileNamesInPreview?: boolean;
-    showFileNames?: boolean;
-    showAlerts?: boolean;
-    clearOnUnmount?: boolean;
-    dropzoneClass?: string;
-    dropzoneParagraphClass?: string;
-    initialFiles?: string[];
-    onChange?: (files: any) => void;
-    onDrop?: (files: any) => void;
-    onDropRejected?: (files: any, evt: any) => void;
-    onDelete?: (file: any) => void;
+  acceptedFiles?: string[];
+  filesLimit?: number;
+  maxFileSize?: number;
+  dropzoneText?: string;
+  showPreviews?: boolean;
+  showPreviewsInDropzone?: boolean;
+  showFileNamesInPreview?: boolean;
+  showFileNames?: boolean;
+  showAlerts?: boolean;
+  clearOnUnmount?: boolean;
+  dropzoneClass?: string;
+  dropzoneParagraphClass?: string;
+  initialFiles?: string[];
+  onChange?: (files: any) => void;
+  onDrop?: (files: any) => void;
+  onDropRejected?: (files: any, evt: any) => void;
+  onDelete?: (file: any) => void;
+  getFileLimitExceedMessage?: (filesLimit: number) => string;
+  getFileAddedMessage?: (fileName: string) => string;
+  getFileRemovedMessage?: (fileName: string) => string;
+  getDropRejectMessage?: (
+    rejectedFile: { name: string; type: string | undefined; size: number },
+    acceptedFiles: string[],
+    maxFileSize: number
+  ) => string;
 }
 export const DropzoneArea: React.ComponentType<DropzoneAreaProps>;
 
 export interface DropzoneDialogProps {
-    open: boolean
-    onSave?: (files: any) => void;
-    onDelete?: (file: any) => void;
-    onClose?: () => void;
-    onChange?: (files: any) => void;
-    onDrop?: (files: any) => void;
-    onDropRejected?: (files: any, evt: any) => void;
-    acceptedFiles?: string[];
-    filesLimit?: number;
-    maxFileSize?: number;
-    dropzoneText?: string;
-    showPreviews?: boolean;
-    showPreviewsInDropzone?: boolean;
-    showAlerts?: boolean;
-    clearOnUnmount?: boolean;
-    dialogTitle?: string;
-    cancelButtonText?: string;
-    submitButtonText?: string;
-    maxWidth?: string;
-    fullWidth?: boolean;
+  open: boolean;
+  onSave?: (files: any) => void;
+  onDelete?: (file: any) => void;
+  onClose?: () => void;
+  onChange?: (files: any) => void;
+  onDrop?: (files: any) => void;
+  onDropRejected?: (files: any, evt: any) => void;
+  acceptedFiles?: string[];
+  filesLimit?: number;
+  maxFileSize?: number;
+  dropzoneText?: string;
+  showPreviews?: boolean;
+  showPreviewsInDropzone?: boolean;
+  showAlerts?: boolean;
+  clearOnUnmount?: boolean;
+  dialogTitle?: string;
+  cancelButtonText?: string;
+  submitButtonText?: string;
+  maxWidth?: string;
+  fullWidth?: boolean;
 }
 export const DropzoneDialog: React.ComponentType<DropzoneDialogProps>;
