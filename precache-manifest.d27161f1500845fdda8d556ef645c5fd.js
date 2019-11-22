13ceac4999917d96d67dcab978b84fcff401a790
yuval <yhalfon@gmail.com>
2019-11-22 08:51:40 +0200

    Updates

---
+++ precache-manifest.d27161f1500845fdda8d556ef645c5fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af95e5a98f30d542df61ca792626e6c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306fa942aecce26b7b1e",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "aa3873bcce94aabef78c",
+    "url": "/static/js/2.970e877e.chunk.js"
+  },
+  {
+    "revision": "306fa942aecce26b7b1e",
+    "url": "/static/js/main.fd8f0b6f.chunk.js"
+  },
+  {
+    "revision": "e3fa2772cda01e73ab7b",
+    "url": "/static/js/runtime-main.21db8ea3.js"
+  }
+]);
