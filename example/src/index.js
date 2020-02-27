39b94fe0adac16283ff2a0d4e617ed9424472ca4
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-02-27 15:35:08 +0100

    Update 'example' code

--- example/src/index.js
+++ example/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 import App from './App'
-import DropzoneDialogExample from "./DropzoneDialogExample";
 
-ReactDOM.render(<DropzoneDialogExample />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
