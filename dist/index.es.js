d41d6aefbf082d2b1ab3508caf9286a769842a4c
panz3r <panz3r@users.noreply.github.com>
2020-04-24 16:46:33 +0000

    :package: Rebuild dist @ 8011c00b48d1c5e9266bf31860f7a430cd2f418f

--- dist/index.es.js
+++ dist/index.es.js
@@ -655,6 +655,7 @@ var DropzoneArea = /*#__PURE__*/function (_React$PureComponent) {
       var _this$props5 = this.props,
           acceptedFiles = _this$props5.acceptedFiles,
           classes = _this$props5.classes,
+          disableRejectionFeedback = _this$props5.disableRejectionFeedback,
           dropzoneClass = _this$props5.dropzoneClass,
           dropzoneParagraphClass = _this$props5.dropzoneParagraphClass,
           dropzoneText = _this$props5.dropzoneText,
@@ -691,7 +692,7 @@ var DropzoneArea = /*#__PURE__*/function (_React$PureComponent) {
             isDragActive = _ref5.isDragActive,
             isDragReject = _ref5.isDragReject;
         return createElement("div", _extends({}, getRootProps(), {
-          className: clsx(classes.dropZone, dropzoneClass, isDragActive && classes.stripes, isDragReject && classes.rejectStripes)
+          className: clsx(classes.dropZone, dropzoneClass, isDragActive && classes.stripes, !disableRejectionFeedback && isDragReject && classes.rejectStripes)
         }), createElement("input", getInputProps()), createElement("div", {
           className: classes.dropzoneTextStyle
         }, createElement(Typography, {
@@ -742,6 +743,7 @@ DropzoneArea.defaultProps = {
   maxFileSize: 3000000,
   dropzoneText: 'Drag and drop a file here or click',
   previewText: 'Preview:',
+  disableRejectionFeedback: false,
   showPreviews: false,
   // By default previews show up under in the dialog and inside in the standalone
   showPreviewsInDropzone: true,
@@ -800,6 +802,9 @@ process.env.NODE_ENV !== "production" ? DropzoneArea.propTypes = {
 
   /** Custom CSS class name for text inside the container. */
   dropzoneParagraphClass: PropTypes.string,
+
+  /** Disable feedback effect when dropping rejected files. */
+  disableRejectionFeedback: PropTypes.bool,
 
   /** Shows previews **BELOW** the dropzone. */
   showPreviews: PropTypes.bool,
