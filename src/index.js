4edd762958b552e7b5a39a9068446ae569ec6787
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-05-26 21:14:33 +0200

    :sparkles: Add controlled version of 'DropzoneArea'- exported as 'DropzoneAreaBase'

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as DropzoneArea} from './components/DropzoneArea';
+export {default as DropzoneAreaBase} from './components/DropzoneAreaBase';
 export {default as DropzoneDialog} from './components/DropzoneDialog';
