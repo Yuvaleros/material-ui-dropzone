7b808cf1e42ccdc4226cb85811aed41d067829fa
Deckstar <dextersibirtsev_ise@yahoo.com>
2021-12-16 11:44:03 +0100

    Improve - better TS compilation settings:
    - made sure "/src/types/index.ts" types were also compiled by removing ".d" from the file name;
    
    - made sure "/src/index.ts" included all export items;
    
    - made sure JSDoc comments were included in the compilation result;
    
    - changed compilation target to use ES6;

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export { default as DropzoneArea } from './components/DropzoneArea';
-export { default as DropzoneAreaBase } from './components/DropzoneAreaBase';
-export { default as DropzoneDialog } from './components/DropzoneDialog';
-export { default as DropzoneDialogBase } from './components/DropzoneDialogBase';
+export { default as DropzoneArea } from "./components/DropzoneArea";
+export * from "./components/DropzoneArea";
+export { default as DropzoneAreaBase } from "./components/DropzoneAreaBase";
+export * from "./components/DropzoneAreaBase";
+export { default as DropzoneDialog } from "./components/DropzoneDialog";
+export * from "./components/DropzoneDialog";
+export { default as DropzoneDialogBase } from "./components/DropzoneDialogBase";
+export * from "./components/DropzoneDialogBase";
+export * from "./helpers";
+export * from "./types";
